8ca6308c3a845a5eedce432beaf4666e75c1c560
Tobias Badertscher <badi@baerospace.ch>
2023-01-09 10:00:28 +0000

    Set up main.

---
+++ main.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  9 10:54:56 2023
+
+Entry point for tge Data logger and Data Emulator
+
+@author: tobias.badertscher
+"""
+def start():
+    print("Start data Logger/Emulator")
+
+
+
+if __name__ == "__main__":
+    start()
+    
