3a1491eb45e69b3a74d615032417a6890e71953e
Tobias Badertscher <tobias.badertscher@noser.com>
2023-02-07 12:43:06 +0100

    Removed res folder

--- gui.py
+++ gui.py
@@ -6,6 +6,7 @@ Created on Mon Feb  6 09:47:21 2023
 """
 import sys, os
 import optparse
+import qrc_resources
 
 sys.path.append(os.sep.join(["C:","ProgramData","Anaconda3","sip"]))
 
@@ -19,6 +20,8 @@ class App(QMainWindow):
         print("Create app")
         super().__init__()
         self.setWindowTitle("DataRecorder")
+        self.status = self.statusBar()
+        self._addMenuBar()
 
         button = QPushButton("Press Me!")
         button.setCheckable(True)
@@ -26,6 +29,27 @@ class App(QMainWindow):
 
         # Set the central widget of the Window.
         self.setCentralWidget(button)
+        
+    def _addMenuBar(self):
+        menu = self.menuBar()
+        self.setMenuBar(menu)
+        fileMenu = QMenu("&File", self)
+        menu.addMenu(fileMenu)
+        saveMenu = QMenu(QIcon(":save.svg"),"&Save", self)
+        menu.addMenu(saveMenu)
+        aboutMenu = QMenu("&About", self)
+        menu.addMenu(aboutMenu)
+       
+    def _createToolBars(self):
+        # Using a title
+        fileToolBar = self.addToolBar("File")
+        # Using a QToolBar object
+        editToolBar = QToolBar("Edit", self)
+        self.addToolBar(editToolBar)
+        # Using a QToolBar object and a toolbar area
+        helpToolBar = QToolBar("Help", self)
+        self.addToolBar(Qt.LeftToolBarArea, helpToolBar)
+
 
     def the_button_was_clicked(self):
         print("Clicked!")
