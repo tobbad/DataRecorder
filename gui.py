d1207d18c75a6b21fc865c40350a51c645dd2cc2
Tobias Badertscher <tobias.badertscher@noser.com>
2023-02-09 13:01:57 +0100

    Update gui.py

--- gui.py
+++ gui.py
@@ -9,6 +9,9 @@ import optparse
 import qrc_resources
 
 sys.path.append(os.sep.join(["C:","ProgramData","Anaconda3","sip"]))
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QVBoxLayout, QLabel, QPushButton
+from PyQt5.QtGui import QIcon, QKeySequence
+from PyQt5.QtCore import pyqtSlot, pyqtSignal, QObject, QThread, QTimer, QTranslator, QLocale
 
 for item in sys.path:
     print("\t %s" % item)
@@ -22,6 +25,9 @@ class App(QMainWindow):
         self.setWindowTitle("DataRecorder")
         self.status = self.statusBar()
         self._addMenuBar()
+        self.dirty = False
+        self.filename = None
+        self.data = None
 
         button = QPushButton("Press Me!")
         button.setCheckable(True)
@@ -29,30 +35,85 @@ class App(QMainWindow):
 
         # Set the central widget of the Window.
         self.setCentralWidget(button)
+        self.sizeLabel = QLabel()
+        
+        self.sizeLabel.setFrameStyle(QFrame.StyledPanel|QFrame.Sunken)
+        status = self.statusBar()
+        status.setSizeGripEnabled(False)
+        status.addPermanentWidget(self.sizeLabel)
+        status.showMessage("Ready", 5000)
         
     def _addMenuBar(self):
-        menu = self.menuBar()
-        self.setMenuBar(menu)
-        fileMenu = QMenu("&File", self)
-        menu.addMenu(fileMenu)
-        saveMenu = QMenu(QIcon(":save.svg"),"&Save", self)
-        menu.addMenu(saveMenu)
-        aboutMenu = QMenu("&About", self)
-        menu.addMenu(aboutMenu)
+        fileOpenAction = self.createAction("&Open...", self.file_open)
+
+        helpAboutAction = self.createAction("&AboutDataRecorder",
+                self.help_about)
+        
+        self.fileMenu = self.menuBar().addMenu("&File")     
+        fileMenueAction=(fileOpenAction,helpAboutAction)
+
+        self.addActions(self.fileMenu,  fileMenueAction)
        
-    def _createToolBars(self):
-        # Using a title
-        fileToolBar = self.addToolBar("File")
-        # Using a QToolBar object
-        editToolBar = QToolBar("Edit", self)
-        self.addToolBar(editToolBar)
-        # Using a QToolBar object and a toolbar area
-        helpToolBar = QToolBar("Help", self)
-        self.addToolBar(Qt.LeftToolBarArea, helpToolBar)
+    def createAction(self, text, slot=None, shortcut=None, icon=None,
+                     tip=None, checkable=False):
+        action = QAction(text, self)
+        if icon is not None:
+            action.setIcon(QIcon(":/{0}.png".format(icon)))
+        if shortcut is not None:
+            action.setShortcut(shortcut)
+        if tip is not None:
+            action.setToolTip(tip)
+            action.setStatusTip(tip)
+        if slot is not None:
+            action.triggered.connect(slot)
+        if checkable:
+            action.setCheckable(True)
+        return action
 
+    def addActions(self, target, actions):
+        for action in actions:
+            if action is None:
+                target.addSeparator()
+            else:
+                target.addAction(action)
 
+    def file_open(self):
+        if not self.okToContinue():
+            return
+        local_dir = (os.path.dirname(self.filename)
+               if self.filename is not None else ".")
+        fmt =  ["CSV Files (*.csv)", "Excel Files (*.xslc)"]
+        files = QFileDialog.getOpenFileName(self,
+                "Load data", local_dir,
+                fmt[0])
+        if files:
+            self.load_file(files)
+        
+    def load_file(self, files):
+        fname, fileType = files
+        f = open(fname)
+        self.data = f.read()
+        f.close()
+        self.filename= fname
+        print("Load file %s  "% (fname))
+            
+    def help_about(self):
+        print("About)")
+        
     def the_button_was_clicked(self):
         print("Clicked!")
+
+    def okToContinue(self):
+        if self.dirty:
+            reply = QMessageBox.question(self,
+                    "Image Changer - Unsaved Changes",
+                    "Save unsaved changes?",
+                    QMessageBox.Yes|QMessageBox.No|QMessageBox.Cancel)
+            if reply == QMessageBox.Cancel:
+                return False
+            elif reply == QMessageBox.Yes:
+                return self.fileSave()
+        return True
 
 
         
@@ -63,12 +124,17 @@ class App(QMainWindow):
 
 if __name__ == "__main__":
     #print("Initalize class")
-    #app =  QApplication(sys.argv)
-
     app  = QApplication(sys.argv)
     window = App()
-    window = App()
+    print(type(App))
+    translator = QTranslator(app)
+
+    defaultLocale = QLocale.system().name()
+    translator.load(defaultLocale)
+    app.installTranslator(translator)
+    print(defaultLocale)
     window.show()
+
     app.exec()
         
     
