0a256f3ce0eaa4d55d95c051e94cc6e4dba05384
Tobias Badertscher <tobias.badertscher@noser.com>
2023-02-13 13:07:13 +0100

    Emulator tab shows recoded data file if loaded befor via File.

--- gui.py
+++ gui.py
@@ -7,14 +7,19 @@ Created on Mon Feb  6 09:47:21 2023
 import sys, os
 import optparse
 import csv
+import numpy as np
 
 import qrc_resources
 
 sys.path.append(os.sep.join(["C:","ProgramData","Anaconda3","sip"]))
+sys.path.append(os.sep.join(["C:","Users","tobias.badertscher","AppData","Local","miniconda3","Lib","site-packages"]))
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QVBoxLayout, QLabel, QPushButton, QStyle
 from PyQt5.QtGui import QIcon, QKeySequence
 from PyQt5.QtCore import pyqtSlot, pyqtSignal, QObject, QThread, QTimer, QTranslator, QLocale
 import pyqtgraph as pg
+import mkl
+print("PYTHONPATH:", os.environ.get('PYTHONPATH'))
+print("PATH:", os.environ.get('PATH'))
     
 from PyQt5.QtWidgets import *
 
@@ -32,7 +37,9 @@ class App(QMainWindow):
         tabWidget = QTabWidget()
         # Add tab widget for Recorder an Emulator
         tabWidget.addTab(self.Recorder(), "Recorder")
-        tabWidget.addTab(self.Emulator(), "Icons")
+        tabWidget.addTab(self.Icons(), "Icons")
+        tabWidget.addTab(self.Emulator(), "Emulator")
+        tabWidget.currentChanged.connect(self.tabChanged)
         # Set the central widget of the Window.
         self.setCentralWidget(tabWidget)
         
@@ -58,7 +65,7 @@ class App(QMainWindow):
                      tip=None, checkable=False):
         action = QAction(text, self)
         if icon is not None:
-            action.setIcon(QIcon(":/{0}.png".format(icon)))
+            action.setIcon(QIcon(":/{0}.svg".format(icon)))
         if shortcut is not None:
             action.setShortcut(shortcut)
         if tip is not None:
@@ -93,14 +100,24 @@ class App(QMainWindow):
         fname, fileType = files
         f = open(fname, encoding="cp1252")
         csvf =csv.reader(f, lineterminator="\n")
-        self.data=[]
-        for line in csvf:
-            self.data.append(line)
+        self.datal=[]
+        for idx, line in enumerate(csvf):
+            time = line[0]
+            relTime= float(line[1])
+            val1 = float(line[2])
+            unit1 = line[3]
+            val2 = float(line[4])
+            unit2 = line[5]
+            self.datal.append([time, relTime, val1, unit1, val2, unit2])
         f.close()
+        self.data = np.zeros([idx+1,2])
+        print("Create numpy array of length %d" % (idx))
         self.filename= fname
         print("Load file %s :"% (self.filename))
-        for line in self.data:
-            print("%s" % line)
+        for idx, line in enumerate(self.datal):
+            self.data[idx][0]  = line[1]
+            self.data[idx][1]  = line[2]
+        print(self.data)
             
     def help_about(self):
         print("About)")
@@ -109,7 +126,8 @@ class App(QMainWindow):
         print("Recorder")
         res = QWidget()
         layout = QVBoxLayout()
-        layout.addWidget(pg.PlotWidget())
+        self.recorderGraph = pg.PlotWidget()
+        layout.addWidget(self.recorderGraph)
         vbox =QHBoxLayout()
         icons = ["Clear","Save","Start","Stop"]
         # for name in icons:
@@ -123,8 +141,8 @@ class App(QMainWindow):
         res.setLayout(layout)  
         return res
 
-    def Emulator(self):
-        print("Emulator")
+    def Icons(self):
+        print("Icon")
         res = QWidget()
         icons = sorted([attr for attr in dir(QStyle) if attr.startswith("SP_")])
         layout = QGridLayout()
@@ -137,10 +155,36 @@ class App(QMainWindow):
             btn.setIcon(icon)
             layout.addWidget(btn, int(n/4), int(n%4))
         res.setLayout(layout)
-        
-        
         return res
+        
+    def Emulator(self):
+        print("Emulator")
+        res = QWidget()
+        layout = QGridLayout()
+        self.emulatorGraph = pg.PlotWidget()
+        layout.addWidget(self.emulatorGraph)
+        res.setLayout(layout)   
+        if self.data is not None:
+            self.updateEmulator()
+        return res
+    
+    def updateEmulator(self):
+        print("Updat emulator graph")
+        if self.data is not None:
+            x = self.data[:,0]
+            y = self.data[:,1]
+            print(x,y)
+            self.emulatorGraph.plot(x,y)
+        
 
+    def tabChanged(self, index):
+        print("Tab changed %d" %(index))
+        if index == 0:
+            pass
+        elif index == 1:
+            pass
+        elif index == 2:
+            self.updateEmulator()
 
     def okToContinue(self):
         if self.dirty:
